0d76b3317575a15ca8741d8eed6f77401a017cf1
Yoshiya Hinosawa <stibium121@gmail.com>
2015-05-13 00:12:02 +0900

    Bump to version v1.2.1

--- actor-system.js
+++ actor-system.js
@@ -1,5 +1,5 @@
 /**
- * actor-system.js v1.2.0
+ * actor-system.js v1.2.1
  * author: Yoshiya Hinosawa ( https://github.com/kt3k )
  * license: MIT
  * depends on jQuery, class-component.js and subclass.js
